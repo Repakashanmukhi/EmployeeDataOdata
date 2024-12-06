ea0c8dac55e0fa43bdf505055738e367e46cc221
Repaka Shanmukhi <shanmukhirepaka@gmail.com>
2024-12-06 06:40:46 +0000

    updated

--- webapp/controller/Odata.controller.js
+++ webapp/controller/Odata.controller.js
@@ -1,53 +1,159 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
-], (Controller,MessageBox) => {
+    "sap/m/MessageBox",
+    "sap/m/MessageToast"
+], (Controller, MessageBox, MessageToast) => {
     "use strict";
-      var that;
+    var that;
     return Controller.extend("empinfo.controller.Odata", {
-        onInit(){
-            that=this;
-            var oModel=that.getOwnerComponent().getModel();
+        onInit() {
+            that = this;
+            var oModel = that.getOwnerComponent().getModel();
             that.getView().setModel(oModel);
-             if(!that.create){
+            if (!that.create) {
                 that.create = sap.ui.xmlfragment("empinfo.fragment.create", that);
-             }
+                that.getView().addDependent(that.create); // Ensure the fragment is added as a dependent
+            } 
         },
-        onOpenDialog: function(){ 
-            that.create.open()
+        onOpenDialog: function () {
+            that.create.open();
         },
-        onSubmitDialog: function(){
-        var sEmpId = sap.ui.getCore().byId("eid").getValue();
-        var sfirstName = sap.ui.getCore().byId("efirstName").getValue();
-        var slastName = sap.ui.getCore().byId("elastName").getValue();
-        var sEmail = sap.ui.getCore().byId("eEmail").getValue();
-        var sPhone = sap.ui.getCore().byId("ePhone").getValue();
-        var sdepartment=sap.ui.getCore().byId("edepartment").getValue();
-        var sposition=sap.ui.getCore().byId("eposition").getValue();
-        var sjoiningDate=sap.ui.getCore().byId("ejoiningDate").getValue();
-        if (sEmpId && sfirstName && slastName && sEmail && sPhone && sdepartment && sposition && sjoiningDate) {
-            var oNewEmployee = {
-                FirstName:sfirstName,
-                LastName:slastName,
-                Email:sEmail,
-                Phone:sPhone,
-                Department:sdepartment,
-                Position:sposition,
-                JoiningDate:sjoiningDate
-            };
-        }
-        var oData = that.getOwnerComponent().getModel();
-        oData.create("/EmployeeInfo", oNewEmployee, {
-                success: function (response) {
-                    MessageToast.show("Record created successfully");
-                   
+        onSubmitDialog: function () {
+            var sfirstName = sap.ui.getCore().byId("efirstName").getValue();
+            var slastName = sap.ui.getCore().byId("elastName").getValue();
+            var sEmail = sap.ui.getCore().byId("eEmail").getValue();
+            var sPhone = sap.ui.getCore().byId("ePhone").getValue();
+            var sdepartment = sap.ui.getCore().byId("edepartment").getValue();
+            var sposition = sap.ui.getCore().byId("eposition").getValue();
+            var sjoiningDate = sap.ui.getCore().byId("ejoiningDate").getValue();
+
+            if (sfirstName && slastName && sEmail && sPhone && sdepartment && sposition && sjoiningDate) {
+                var oNewEmployee = {
+                    FirstName: sfirstName,
+                    LastName: slastName,
+                    Email: sEmail,
+                    Phone: sPhone,
+                    Department: sdepartment,
+                    Position: sposition,
+                    JoiningDate: sjoiningDate
+                };
+
+                var oData = that.getOwnerComponent().getModel();
+
+                // Creating the new employee record
+                oData.create("/EmployeeInfo", oNewEmployee, {
+                    success: function (response) {
+                        console.log(response)
+                        // Show success message
+                        MessageToast.show("Record created successfully");
+                        // Close the dialog
+                        that.create.close();
+
+                        // Refresh the table data (ensure the model is refreshed)
+                        oData.refresh(true);
+                    },
+                    error: function (error) {
+                        // Handle error if the creation fails
+                        MessageToast.show("Error creating record");
+                        console.log(error);
+                    }
+                });
+            } else {
+                MessageBox.error("Please fill all required fields.");
+            }
+        },
+        // Close the dialog function if needed
+        onclose: function () {
+            that.create.close();
+        },
+        // onClear: function(){
+        //     sap.ui.getCore().byId("eFirstName").setValue("");
+        //     sap.ui.getCore().byId("eLatName").setValue("");
+        //     sap.ui.getCore().byId("eEmail").setValue("");
+        //     sap.ui.getCore().byId("ePhone").setValue("");
+        //     sap.ui.getCore().byId("edepartment").setValue("");
+        //     sap.ui.getCore().byId("eposition").setValue("");
+        //     sap.ui.getCore().byId("ejoiningDate").setValue("");
+        // } 
+        DeleteBtn: function(oEvent){
+            var oButton=oEvent.getSource();
+            var oContext=oButton.getBindingContext();
+            var sPath=oContext.getPath();
+            var oModel=this.getView().getModel();
+            oModel.remove(sPath,{
+                success: function(){
+                    sap.m.MessageToast.show("Record deleted successfully!");
                 },
-                error: function (error) {
-                    console.log(error)
+                error: function(){
+                    sap.m.MessageToast.show("Cannot delete record");
                 }
-        })
- 
-    },            
-        })
-    })
-   
+            })
+        },
+        UpdateBtn: function(oEvent){
+            if(!that.update){
+                that.update=sap.ui.xmlfragment("empinfo.fragment.update", that)
+            }
+            var oContext = oEvent.getSource().getBindingContext().getObject();
+            sap.ui.getCore().byId("FN_E").setValue(oContext.FirstName);
+            sap.ui.getCore().byId("LN_E").setValue(oContext.LastName);
+            sap.ui.getCore().byId("E_E").setValue(oContext.Email);
+            sap.ui.getCore().byId("P_E").setValue(oContext.Phone);
+            sap.ui.getCore().byId("D_E").setValue(oContext.Department);
+            sap.ui.getCore().byId("PO_E").setValue(oContext.Position);
+            sap.ui.getCore().byId("JD_E").setValue(oContext.JoiningDate);
+            that.update.open();
+        },
+        onUpdateDialog: function(oEvent) {
+            // Get the updated values from the dialog fields
+            var sfirstName = sap.ui.getCore().byId("FN_E").getValue();
+            var slastName = sap.ui.getCore().byId("LN_E").getValue();
+            var sEmail = sap.ui.getCore().byId("E_E").getValue();
+            var sPhone = sap.ui.getCore().byId("P_E").getValue();
+            var sDepartment = sap.ui.getCore().byId("D_E").getValue();
+            var sPosition = sap.ui.getCore().byId("PO_E").getValue();
+            var sJoiningDate = sap.ui.getCore().byId("JD_E").getValue();
+        
+            // Validate that all fields are filled
+            if (sfirstName && slastName && sEmail && sPhone && sDepartment && sPosition && sJoiningDate) {
+                var oUpdatedEmployee = {
+                    FirstName: sfirstName,
+                    LastName: slastName,
+                    Email: sEmail,
+                    Phone: sPhone,
+                    Department: sDepartment,
+                    Position: sPosition,
+                    JoiningDate: sJoiningDate
+                };
+                var oData = that.getOwnerComponent().getModel();
+
+                // Updating the new employee record
+                oData.update("/EmployeeInfo", oUpdatedEmployee, {
+                    success: function (response) {
+                        console.log(response)
+                        // Show success message
+                        MessageToast.show("Record updated successfully");
+                        // Close the dialog
+                        that.create.close();
+
+                        // Refresh the table data (ensure the model is refreshed)
+                        // oData.refresh(true);
+                    },
+                    error: function (error) {
+                        // Handle error if the updation fails
+                        MessageToast.show("Error updating record");
+                        console.log(error);
+                    }
+                });
+            } else {
+                MessageBox.error("Please fill all required fields.");
+            }
+        },
+        // Close the dialog function if needed
+        onCancleDialog: function () {
+            that.create.close();
+        },
+    })           
+});
+  
+
+
